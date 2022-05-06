8afb57b7f978b7843db21c069b3a67b2a2644fbd
Liam Weitzel <liam.weitzel@gmail.com>
2022-05-07 00:38:49 +0100

    Add texturing to background quad using SOIL

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <GL/Glut.h>
+/*#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <iostream>
 #include <stdlib.h>
 
@@ -67,7 +68,7 @@ void display(void) {
 	dot_x[0] = first_dot_x;
 	dot_y[0] = first_dot_y;
 
-	for (int k = 0; k <= i-1; k++) {
+	for (int k = 0; k <= i - 1; k++) {
 
 		glColor3f(1, 0, 0);
 		glPointSize(1);
@@ -83,6 +84,9 @@ void display(void) {
 		glVertex2i(dot_x[k], dot_y[k]);
 
 		glEnd();
+
+		std::cout << "Point: " << k << ", x: " << dot_x[k] << ", y: " << dot_y[k] << "\n";
+
 	}
 
 	glFlush();
@@ -100,6 +104,93 @@ int main(int argc, char** argv) {
 
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
+	glutMainLoop();
+	return 0;
+}*/
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+#include <SOIL/SOIL.h>
+
+GLuint texture;
+
+GLuint glInitTexture(const char* filename)
+{
+	FILE* f = NULL;
+	f = fopen(filename, "r");
+	if (f == NULL)
+	{
+		fprintf(stderr, "cannot load file %s!", filename);
+		exit(1);
+	}
+	GLuint t = SOIL_load_OGL_texture(filename, SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+
+	glBindTexture(GL_TEXTURE_2D, t);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	return t;
+}
+
+void drawImage(GLuint file,
+	float x,
+	float y,
+	float w,
+	float h,
+	float angle)
+{
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glPushMatrix();
+	glTranslatef(x, y, 0.0);
+	glRotatef(angle, 0.0, 0.0, 1.0);
+
+	glBindTexture(GL_TEXTURE_2D, file);
+	glEnable(GL_TEXTURE_2D);
+
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0); glVertex3f(x, y, 0.0f);
+	glTexCoord2f(0.0, 2.0); glVertex3f(x, y + h, 0.0f);
+	glTexCoord2f(2.0, 2.0); glVertex3f(x + w, y + h, 0.0f);
+	glTexCoord2f(2.0, 0.0); glVertex3f(x + w, y, 0.0f);
+	glEnd();
+
+	glPopMatrix();
+}
+
+void render()
+{
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	const double w = glutGet(GLUT_WINDOW_WIDTH);
+	const double h = glutGet(GLUT_WINDOW_HEIGHT);
+	gluPerspective(45.0, w / h, 0.1, 1000.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0, 0, -5);
+
+	drawImage(texture, -1.18f, -1.01f, 9.35f, 8.2f, 0.0);
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(1200, 1200);
+	glutCreateWindow("Applying Textures");
+	glutDisplayFunc(render);
+
+	texture = glInitTexture("image.png");
+
 	glutMainLoop();
 	return 0;
 }
