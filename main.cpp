631bacaa7c1bfe79e261476c3e6fb36880976f11
Liam Weitzel <liam.weitzel@gmail.com>
2022-05-07 01:00:59 +0100

    Implement area selection ontop of quad background

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
-/*#include <GL/glew.h>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <iostream>
 #include <stdlib.h>
+#include <SOIL/SOIL.h>
+
+GLuint texture;
 
 int i = 1;
 int line_x[100], line_y[100];
 int dot_x[100], dot_y[100];
 int first_dot_x, first_dot_y, first_line_x, first_line_y = -1;
 
-int width = 640;
-int height = 480;
-float width_f = 640;
-float height_f = 480;
+int width = 1000;
+int height = 1000;
+float width_f = 1000;
+float height_f = 1000;
 
 void mouse(int button, int state, int mousex, int mousey) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
@@ -36,85 +40,6 @@ void mouse(int button, int state, int mousex, int mousey) {
 	}
 	glutPostRedisplay();
 }
-
-void display(void) {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, width_f, 0.0, height_f);
-
-	//Drawing the background quad:
-	glPushMatrix();
-	glLoadIdentity();
-
-	glColor3f(0, 1, 0);
-	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0);
-	glVertex2f(-1.0f, -1.0f);
-	glTexCoord2f(1, 0);
-	glVertex2f(1.0f, -1.0f);
-	glTexCoord2f(1, 1);
-	glVertex2f(1.0f, 1.0f);
-	glTexCoord2f(0, 1);
-	glVertex2f(-1.0f, 1.0f);
-	glEnd();
-
-	glPopMatrix();
-
-	//Drawing the dots & lines
-	line_x[i] = first_line_x;
-	line_y[i] = first_line_y;
-	line_y[0] = first_line_y;
-	line_x[0] = first_line_x;
-	dot_x[0] = first_dot_x;
-	dot_y[0] = first_dot_y;
-
-	for (int k = 0; k <= i - 1; k++) {
-
-		glColor3f(1, 0, 0);
-		glPointSize(1);
-		glBegin(GL_LINES);
-		glVertex2i(line_x[k], line_y[k]);
-		glVertex2i(line_x[k + 1], line_y[k + 1]);
-
-		glEnd();
-
-		glColor3f(1, 0, 0);
-		glPointSize(5);
-		glBegin(GL_POINTS);
-		glVertex2i(dot_x[k], dot_y[k]);
-
-		glEnd();
-
-		std::cout << "Point: " << k << ", x: " << dot_x[k] << ", y: " << dot_y[k] << "\n";
-
-	}
-
-	glFlush();
-}
-
-int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(width, height);
-	glutInitWindowPosition(10, 10);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutCreateWindow("Tree Farm Manager");
-
-	glClearColor(1, 1, 1, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glutDisplayFunc(display);
-	glutMouseFunc(mouse);
-	glutMainLoop();
-	return 0;
-}*/
-
-#define _CRT_SECURE_NO_DEPRECATE
-#include <stdlib.h>
-#include <stdio.h>
-#include <GL/glut.h>
-#include <SOIL/SOIL.h>
-
-GLuint texture;
 
 GLuint glInitTexture(const char* filename)
 {
@@ -173,10 +98,45 @@ void render()
 	gluPerspective(45.0, w / h, 0.1, 1000.0);
 
 	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
 	glLoadIdentity();
 	glTranslatef(0, 0, -5);
 
-	drawImage(texture, -1.18f, -1.01f, 9.35f, 8.2f, 0.0);
+	drawImage(texture, -1.05f, -1.05f, 8.3f, 8.3f, 0.0);
+	glPopMatrix();
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, width_f, 0.0, height_f);
+
+	//Drawing the dots & lines
+	line_x[i] = first_line_x;
+	line_y[i] = first_line_y;
+	line_y[0] = first_line_y;
+	line_x[0] = first_line_x;
+	dot_x[0] = first_dot_x;
+	dot_y[0] = first_dot_y;
+
+	for (int k = 0; k <= i - 1; k++) {
+
+		glPointSize(1);
+		glBegin(GL_LINES);
+		glVertex2i(line_x[k], line_y[k]);
+		glVertex2i(line_x[k + 1], line_y[k + 1]);
+
+		glEnd();
+
+		glPointSize(5);
+		glBegin(GL_POINTS);
+		glVertex2i(dot_x[k], dot_y[k]);
+
+		glEnd();
+
+		//std::cout << "Point: " << k << ", x: " << dot_x[k] << ", y: " << dot_y[k] << "\n";
+
+	}
+
+	glFlush();
 
 	glutSwapBuffers();
 }
@@ -185,9 +145,10 @@ int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1200, 1200);
-	glutCreateWindow("Applying Textures");
+	glutInitWindowSize(1000, 1000);
+	glutCreateWindow("Tree Farm Manager");
 	glutDisplayFunc(render);
+	glutMouseFunc(mouse);
 
 	texture = glInitTexture("image.png");
 
